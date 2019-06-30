06100283d36ce6fb31a7f6d5e6290c484c47d466
Akiko Takano <akiko.pusu@gmail.com>
2019-06-30 16:38:47 +0900

    Add comment: This app.js is written based on CommonJS.

--- react-ssr/app.js
+++ react-ssr/app.js
@@ -1,9 +1,11 @@
-const express = require('express')
+// require('module-name') is the CommonJS's style.
+// This need not to transpile and run by Node.js directly.
+const express = require("express")
 const app = express()
 const port = 3000
 
 const now = new Date()
 
-app.get('/', (req, res) => res.send(`Hello World! Server started: ${now}`))
+app.get("/", (req, res) => res.send(`Hello World! Server started: ${now}`))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
