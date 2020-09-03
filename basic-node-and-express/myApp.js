ce8e3208dc562a3efc8613e3fed96d7f7dfb4aa7
Akiko Takano <akiko.pusu@gmail.com>
2020-09-03 23:32:41 +0900

    Update myApp.js

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -8,4 +8,4 @@ module.exports = app
 // https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/meet-the-node-console
 // Modify the myApp.js file to log "Hello World" to the console.
 // server.js から読み込まれます (もしくは単体で node myApp.js を実行したら 'Hello World' と出力)
-console.log('Hello World')
+console.log("Hello World")
