7ec04fb62c8183cf129cc118f3dc258f8fb51e38
Akiko Takano <akiko.pusu@gmail.com>
2020-09-05 17:50:06 +0900

    Add scripts for Basic Node and Express Challange of freeCodeCamp.

--- basic-node-and-express/myApp.js
+++ basic-node-and-express/myApp.js
@@ -1,11 +1,65 @@
-// File for Basic Node and Express
-// main App (この中でもExpressを使います)
-var express = require('express')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
 
 module.exports = app
-
-// https://www.freecodecamp.org/learn/apis-and-microservices/basic-node-and-express/meet-the-node-console
-// Modify the myApp.js file to log "Hello World" to the console.
-// server.js から読み込まれます (もしくは単体で node myApp.js を実行したら 'Hello World' と出力)
 console.log('Hello World')
+
+const absolutePath = path.join(__dirname, '/views/index.html')
+const assetsPath = path.join(__dirname, '/public') // ここを静的ファイルとして読み込ませたい
+
+app.use(express.static(assetsPath))
+
+// body-parserを適用する
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function (req, res, next) {
+  // req.method, req.path and req.ip
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+  res.sendFile(absolutePath)
+})
+
+app.get('/json', function (req, res) {
+  let message = 'Hello json'
+
+  if (process.env.MESSAGE_STYLE == 'uppercase') {
+    message = 'HELLO JSON'
+  }
+
+  const obj = { message: message }
+  // pass the Object as argument
+  res.json(obj)
+})
+
+// Chain Middleware to Create a Time Server
+app.get(
+  '/now',
+  function (req, res, next) {
+    req.time = new Date().toString() // Hypothetical synchronous operation
+    next()
+  },
+  function (req, res) {
+    res.send({ time: req.time })
+  }
+)
+
+// Chain Middleware to Create a Time Server
+app.get('/:word/echo', function (req, res) {
+  res.send({ echo: req.params.word })
+})
+
+// Chain Middleware to Create a Time Server
+app
+  .get('/name', function (req, res) {
+    res.send({ name: `${req.query.first} ${req.query.last}` })
+  })
+  .post('/name', function (req, res) {
+    res.send({ name: `${req.body.first} ${req.body.last}` })
+  })
--- basic-node-and-express/server.js
+++ basic-node-and-express/server.js
@@ -1,9 +1,11 @@
 // File for Basic Node and Express
-var myApp = require('./myApp')
-var express = require('express')
-var app = express()
+const app = require('./myApp')
+
+// dotenvを有効にする指定
+require('dotenv').config()
 
 // これがないと起動しません
+// node server.js
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
