c72332ef7057c88829ad45ef92b5ff218f5a814e
Akiko Takano <akiko.pusu@gmail.com>
2020-10-14 08:19:02 +0900

    Update file and add template for Advanced Node and Express - Use a Template Engine's Powers.

--- advanced-node-and-express/myApp.js
+++ advanced-node-and-express/myApp.js
@@ -1,10 +1,14 @@
 const pug = require('pug')
-const compiledFunction = pug.compileFile('template.pug')
+const compiledFunction = pug.compileFile('views/template.pug')
 
 module.exports = (app) => {
   app.set('view engine', 'pug')
 
+  app.get('/pug', (req, res) => {
+    res.send('Hello World! I\'m from Japan! ' + compiledFunction({ name: 'Shiba-Wanko', message: 'Test....' }))
+  })
+
   app.get('/', (req, res) => {
-    res.send(`Hello World! I'm from Japan! ` + compiledFunction({ name: 'Shiba-Wanko' }))
+    res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' })
   })
 }
